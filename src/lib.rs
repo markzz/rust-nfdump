71353f0cf354e72281bcd3def34329130b2ee247
Mark King <mark.king@markzz.com>
2023-11-13 23:53:48 -0500

    remove rustrover starter code

--- src/lib.rs
+++ src/lib.rs
@@ -381,18 +381,3 @@ impl<R: Read> NfFileReaderV1<R> {
         }
     }
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
